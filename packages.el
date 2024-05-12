0f31ece32b673af77fe4a4a0a71711636f87e468
zhao <jazhawang@gmail.com>
2024-05-12 11:05:41 -0400

    update packages... once again

--- packages.el
+++ packages.el
@@ -4,11 +4,9 @@
       '(("gnu" . "http://elpa.gnu.org/packages/")
         ("melpa" . "http://melpa.org/packages/")))
 
-
 (defvar required-packages
   '(
     avy
-    anzu
     ace-window    
     flyspell
     solarized-theme
@@ -21,13 +19,10 @@
     use-package
     vterm
     magit
-    projectile
     lsp-mode
     arduino-mode
-    treemacs
-    treemacs-projectile
     scad-mode
-    company
+    company    
     )
   "A list of required packages. Will be either installed or updated at launch")
 
@@ -55,24 +50,7 @@
   
 (require 'jack-mode)
 
-
-;; (require 'helm)
-
-;; (helm-mode 1)
-;; (setq helm-M-x-fuzzy-match t) ;; enable fuzzy matching for helm-M-x
-
-;; (setq helm-buffers-fuzzy-matching t
-;;       helm-recentf-fuzzy-match t) ;; fuzzy matching for helm-mini
-
-;; (define-key jack-mode-map (kbd "C-c h") 'helm-command-prefix)
-;; (define-key jack-mode-map [remap apropos-command] 'helm-apropos)
-;; (define-key jack-mode-map [remap execute-extended-command] 'helm-M-x)
-;; (define-key jack-mode-map [remap find-file] 'helm-find-files)
-;; (define-key jack-mode-map [remap switch-to-buffer] 'helm-mini)
-
-
 (defvar jack-main-row-keys '(?a ?o ?e ?u ?i ?d ?h ?t ?n ?s))
-
 (require 'ace-window)
 (setq aw-keys jack-main-row-keys)
 (define-key jack-mode-map [remap other-window] 'ace-select-window)
@@ -83,10 +61,6 @@
 (setq avy-keys jack-main-row-keys)
 (define-key jack-mode-map (kbd "C-c C-g") 'avy-goto-line)
 (define-key jack-mode-map (kbd "C-c C-c") 'avy-goto-char)
-
-
-(require 'anzu)
-(global-anzu-mode 1)
 
 
 (require 'flyspell)
@@ -158,12 +132,8 @@
   :commands lsp)
 
 
-(require 'treemacs)
-
-
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
-
 
 (use-package casual-dired
   :ensure t
@@ -175,7 +145,128 @@
   :init
   (vertico-mode))
 
-;; (use-package savehist                  
-;;   :init
-;;   (savehist-mode))
+(use-package savehist
+  :ensure t
+  :init
+  (savehist-mode))
 
+
+(use-package marginalia
+  :ensure t
+  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
+  :bind (:map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+  :init
+  (marginalia-mode))
+
+;; Example configuration for Consult
+(use-package consult
+  :ensure t
+
+  :bind (("C-s" . consult-line)
+         ("C-h C-m" . consult-man)
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)         
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history)                 ;; orig. previous-matching-history-element
+         )
+  
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key "M-."
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+)
+
+
+(use-package orderless
+ :ensure t
+ :init
+ (setq completion-styles '(orderless basic)
+   completion-category-defaults nil
+   completion-category-overrides '((file (styles . (partial-completion))))))  
